1e37ba755758d09eefb6014ca2eb822278653235
Андрей Серховец <serhovetc88@gmail.com>
2020-11-03 13:05:15 +0200

    add new homework

---
+++ js-2-Andrey/js/script.js
@@ -0,0 +1,21 @@
+let words = [
+    rus = ["Привет", "Медведь", "Облоко"],
+    eng = ["Hello", "Bear", "Cloud"],
+];
+
+console.log(words[1]);
+
+let rusBtn = document.querySelector('.btn--rus');
+let engBtn = document.querySelector('.btn--eng');
+let resetBtn = document.querySelector('.btn--reset')
+
+rusBtn.addEventListener("click", function () {
+    console.log(words[0]);
+})
+engBtn.addEventListener("click", function () {
+    console.log(words[1]);
+})
+resetBtn.addEventListener("click", function () {
+    console.clear();
+})
+
