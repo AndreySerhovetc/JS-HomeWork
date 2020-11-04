8d31e63d49fafd7e7c93ad10a7ff7a7bd98aba83
Андрей Серховец <serhovetc88@gmail.com>
2020-11-04 12:28:49 +0200

    add cycle for

--- js-2-Andrey/js/script.js
+++ js-2-Andrey/js/script.js
@@ -9,10 +9,18 @@ let engBtn = document.querySelector('.btn--eng');
 let resetBtn = document.querySelector('.btn--reset')
 
 rusBtn.addEventListener("click", function () {
-    console.log(words[0]);
+    for(let i = 0; i < words.length; i++){
+        for(let y = 0; y < words[i].length; y++){
+            console.log(words[0][0], words[0][1], words[0][2] );
+        }
+    }
 })
 engBtn.addEventListener("click", function () {
-    console.log(words[1]);
+    for(let i = 0; i < words.length; i++){
+        for(let y = 0; y < words[i].length; y++){
+            console.log(words[1][0], words[1][1], words[1][2] );
+        }
+    }
 })
 resetBtn.addEventListener("click", function () {
     console.clear();
