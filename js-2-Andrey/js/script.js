bbbc631e3b9a954a42e2c076762686cd42f38a99
Андрей Серховец <serhovetc88@gmail.com>
2020-11-04 19:17:15 +0200

    forEach add

--- js-2-Andrey/js/script.js
+++ js-2-Andrey/js/script.js
@@ -1,7 +1,8 @@
-let words = [
-    rus = ["Привет", "Медведь", "Облоко"],
-    eng = ["Hello", "Bear", "Cloud"],
-];
+let words = {
+    "rus": ["Привет", "Медведь", "Облоко"],
+    "eng": ["Hello", "Bear", "Cloud"],
+}
+   
 
 
 let rusBtn = document.querySelector('.btn--rus');
@@ -9,19 +10,17 @@ let engBtn = document.querySelector('.btn--eng');
 let resetBtn = document.querySelector('.btn--reset')
 
 rusBtn.addEventListener("click", function () {
-    for(let i = 0; i < words.length; i++){
-        for(let y = 0; y < words[i].length; y++){
-            console.log(words[0][0], words[0][1], words[0][2] );
-        }
-    }
-})
+    words["rus"].forEach(function(elem){
+        console.log(elem);
+    });
+});
+
 engBtn.addEventListener("click", function () {
-    for(let i = 0; i < words.length; i++){
-        for(let y = 0; y < words[i].length; y++){
-            console.log(words[1][0], words[1][1], words[1][2] );
-        }
-    }
-})
+    words["eng"].forEach(function(elem){
+        console.log(elem);
+    });
+});
+
 resetBtn.addEventListener("click", function () {
     console.clear();
 })
