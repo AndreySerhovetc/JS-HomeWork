9003e2f3d5c36d10f7e819d99a692c7eebeeef40
Андрей Серховец <serhovetc88@gmail.com>
2020-12-03 13:14:05 +0200

    fix animation

--- js-6-Andrey/js/script.js
+++ js-6-Andrey/js/script.js
@@ -1,30 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
-   let tabContent = document.querySelectorAll(".content");
-   let tabControl = document.querySelectorAll(".btn__item")
 
-    tabControl.forEach(elem =>{
-        elem.addEventListener("click", function(){
-            showTabContent(elem.dataset.tabnumber);
-            tabControl.forEach(elem => {
-                elem.classList.remove("btn__item--active");
-            })
-            this.classList.add("btn__item--active");
-        })
-        
-    });
-
-   function hideTabContent() {
-       for (let i = 1; i < tabContent.length; i++) {
-           tabContent[i].classList.add("content--hidden");
-           
-       }
-   }
-   hideTabContent();
-
-   function showTabContent(tabnumber) {
-    for (let i = 0; i < tabContent.length; i++) {
-        tabContent[i].classList.add("content--hidden");
+    let tabContent = document.querySelectorAll(".content");
+    let tabControl = document.querySelectorAll(".btn__item")
+ 
+     tabControl.forEach(elem =>{
+         elem.addEventListener("click", function(){
+             showTabContent(elem.dataset.tabnumber);
+             tabControl.forEach(elem => {
+                 elem.classList.remove("btn__item--active");
+             })
+             this.classList.add("btn__item--active");
+         })
+         
+     });
+ 
+    function hideTabContent() {
+        for (let i = 1; i < tabContent.length; i++) {
+            tabContent[i].classList.add("content--hidden");
+            tabContent[i].classList.remove("scrollright");
+            
+        }
     }
-    tabContent[tabnumber - 1].classList.remove("content--hidden");
-   }
+    hideTabContent();
+ 
+    function showTabContent(tabnumber) {
+     for (let i = 0; i < tabContent.length; i++) {
+         tabContent[i].classList.add("content--hidden");
+         
+     }
+     tabContent[tabnumber - 1].classList.remove("content--hidden");
+     tabContent[tabnumber - 1].classList.add("scrollright");
+    }
+
 });
